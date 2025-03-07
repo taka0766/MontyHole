b644656454e111d17cac936733bf5d891ca59495
taka0766 <s2000.4wk@gmail.com>
2025-03-07 22:08:20 +0900

    変数名の変更: door を doors に変更 メソッド名のスネークケース化: getLength などを get_length に変更 型ヒントの追加: 引数と戻り値の型ヒントを追加 Docstring の修正: Docstring の内容を修正

--- Pandora.py
+++ Pandora.py
@@ -1,67 +1,53 @@
 import random
 
 class Pandora:
-    """
-    複数の扉の情報を管理するクラス
-    """
+    """複数の扉の情報を管理するクラス"""
 
-    def __init__(self, length:int=3):
+    def __init__(self, length: int = 3):
+        """初期化"""
         # 回答可能なドアの総数
-        self.__door = list(range(length))
+        self.__doors = list(range(length))
         # 今回の正解のドアの番号をランダムに生成
         self.__answer = random.randrange(length)
-
         self.__player_answer = None
+        self.__open_door = None
 
-    def getLength(self):
-        """
-        回答可能なドアの総数を返す。
-        """
-        return len(self.__door)
+    def get_length(self) -> int:
+        """回答可能なドアの総数を返す。"""
+        return len(self.__doors)
 
-    def setPlayerAnswer(self, answer):
-        """
-        Playerが選択した回答を設定
-        """
+    def set_player_answer(self, answer: int) -> None:
+        """Playerが選択した回答を設定"""
         self.__player_answer = answer
 
-    def getRemaining(self):
-        """
-        選択されていないドアの中で、ハズレであるドアの添字をリストで返す
-        """
-        tmp_door = self.__door.copy()
+    def get_remaining(self) -> list[int]:
+        """選択されていないドアの中で、ハズレであるドアの添字をリストで返す"""
+        tmp_doors = self.__doors.copy()
         # Playerが選択したドアを除外する
-        tmp_door.remove(self.__player_answer)
+        tmp_doors.remove(self.__player_answer)
         # 残りのドアに回答のドアが存在する場合は、除外する
-        if self.__answer in tmp_door:
-            tmp_door.remove(self.__answer)
+        if self.__answer in tmp_doors:
+            tmp_doors.remove(self.__answer)
+        return tmp_doors
 
-        return tmp_door
-
-    def setOpenDoor(self, open_door:int):
-        """
-        ハズレのドアを開く
-        """
+    def set_open_door(self, open_door: int) -> None:
+        """ハズレのドアを開く"""
         if self.__answer == open_door:
             raise ValueError("正解の扉を開くことはできません")
         if self.__player_answer == open_door:
             raise ValueError("Playerが選択した扉を開くことはできません")
         self.__open_door = open_door
 
-    def getSelectableDoor(self):
-        """
-        選択可能なドアを返す。
-        """
+    def get_selectable_door(self) -> list[int]:
+        """選択可能なドアを返す。"""
         # 回答可能な全てのドアを取得する。
-        tmp_door = self.__door.copy()
+        tmp_doors = self.__doors.copy()
         # すでにPlayerが選択したものを除外
-        tmp_door.remove(self.__player_answer)
+        tmp_doors.remove(self.__player_answer)
         # すでに開けられているドアを除外
-        tmp_door.remove(self.__open_door)
-        return tmp_door
+        tmp_doors.remove(self.__open_door)
+        return tmp_doors
 
-    def isCorrect(self):
-        """
-        Playerの選択が正解か否かを判定する
-        """
+    def is_correct(self) -> bool:
+        """Playerの選択が正解か否かを判定する"""
         return self.__answer == self.__player_answer
