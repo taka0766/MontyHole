fb43abcd03361003019f6c80c435b2e85104052d
taka0766 <s2000.4wk@gmail.com>
2025-03-07 22:09:42 +0900

    メソッド名のスネークケース化: selectFirstAnswer などを select_first_answer に変更 型ヒントの追加: 引数と戻り値の型ヒントを追加 Docstring の修正: Docstring の内容を修正

--- Player.py
+++ Player.py
@@ -1,33 +1,26 @@
 import random
 import Pandora
 
-class Player :
-    """
-    ゲームのプレイヤークラス
-    """
+class Player:
+    """ゲームのプレイヤークラス"""
 
-    def __init__(self) :
+    def __init__(self):
+        """初期化"""
         # Playerが選択した正解の値
         self.__answer = None
 
-    def selectFirstAnswer(self, pandora:Pandora):
-        """
-        初回の回答を選択する。（回答可能の要素からランダムに一つ選択する。）
-        """
+    def select_first_answer(self, pandora: Pandora.Pandora) -> None:
+        """初回の回答を選択する。（回答可能の要素からランダムに一つ選択する。）"""
         # Pandoraの長さ（回答の選択肢）を取得
-        length = pandora.getLength()
-
+        length = pandora.get_length()
         # 乱数から初回の選択肢を生成
         self.__answer = random.randrange(length)
-
         # Pandoraに回答を設定
-        pandora.setPlayerAnswer(self.__answer)
+        pandora.set_player_answer(self.__answer)
 
-    def selectChangeAnswer(self, pandora:Pandora):
-        """
-        Montyが開いたドアの番号を受けて、自身の回答を残りのドアに変更する。
-        """
-        door = pandora.getSelectableDoor()
+    def select_change_answer(self, pandora: Pandora.Pandora) -> None:
+        """Montyが開いたドアの番号を受けて、自身の回答を残りのドアに変更する。"""
+        door = pandora.get_selectable_door()
         # Pandoraに変更した回答を設定
         self.__answer = random.choice(door)
-        pandora.setPlayerAnswer(self.__answer)
+        pandora.set_player_answer(self.__answer)
