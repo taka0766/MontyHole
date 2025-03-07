1b10c790b3d612a98b3c5afefb3d1d3a959406e5
taka0766 <s2000.4wk@gmail.com>
2025-03-07 22:07:03 +0900

    メソッド名のスネークケース化: visualizeLosing を visualize_losing に変更

--- Monty.py
+++ Monty.py
@@ -1,17 +1,11 @@
 import random
 import Pandora
 
-class Monty :
-    """
-    ゲームの司会者（Monty）クラス
-    """
-    def __init__(self):
-        pass
+class Monty:
+    """ゲームの司会者（Monty）クラス"""
 
-    def visualizeLosing(self, pandora:Pandora):
-        """
-        残りの扉から、ハズレの扉を開ける。
-        """
-        losing_list = pandora.getRemaining()
+    def visualize_losing(self, pandora: Pandora.Pandora):
+        """残りの扉から、ハズレの扉を開ける。"""
+        losing_list = pandora.get_remaining()
         open_door = random.choice(losing_list)
-        pandora.setOpenDoor(open_door)
+        pandora.set_open_door(open_door)
